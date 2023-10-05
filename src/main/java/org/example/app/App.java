c9e2546c0ac61b9ee995fa518e25e4b8fa73849c
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-05 19:05:56 +0300

    Add main method

---
+++ src/main/java/org/example/app/App.java
@@ -0,0 +1,11 @@
+package org.example.app;
+
+import org.example.app.service.AppStarter;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String... args) throws IOException {
+        AppStarter.runApp();
+    }
+}
