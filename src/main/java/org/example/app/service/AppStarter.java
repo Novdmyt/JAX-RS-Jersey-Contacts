f8ca9da63aac0989edd1bf6705b23b000005984a
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-05 19:05:36 +0300

    Add method runApp

---
+++ src/main/java/org/example/app/service/AppStarter.java
@@ -0,0 +1,19 @@
+package org.example.app.service;
+
+import jakarta.ws.rs.core.UriBuilder;
+import org.example.app.utils.Constants;
+import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class AppStarter {
+
+    public static void runApp() throws MalformedURLException {
+        URI baseUri = UriBuilder.fromUri(Constants.URI_TEMPLATE).port(Constants.PORT).build();
+        ResourceConfig resourceConfig = new ResourceConfig().packages(Constants.PACKS);
+        NettyHttpContainerProvider.createServer(baseUri, resourceConfig, false);
+        System.out.printf("Application running on %s\n", baseUri.toURL().toExternalForm());
+    }
+}
